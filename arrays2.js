29d3c6cce5d08db61f4b88d835706b03faf258e9
Hari Shankar Limbu <67302527+ashoklimbu10@users.noreply.github.com>
2025-05-14 22:25:27 +0545

    Add files via upload

---
+++ arrays2.js
@@ -0,0 +1,78 @@
+// .splice method - add or removes from middle of array
+// let names=['gerige', 'ram','sita'];
+// let theGod=names.splice(1,1);
+// console.log(names);
+// console.log(theGod); 
+
+// let names=['geroge', 'paul' , 'lisa ', 'ram','sita'];
+// // let newGroup=names.splice(2,2,'pete', 'nimit');
+// let newgroup=names.splice(2,0,'john');
+// console.log(names);
+// console.log(newGroup);
+
+
+// join method
+// let beatless=['geroge', 'paul' , 'lisa ', 'ram','sita'];
+// let sentence='the beatless were ' +beatless;
+// console.log(sentence); 
+// not much costumizable
+// let beatless=['geroge', 'paul' , 'lisa ', 'ram','sita'];
+// let sentence='the beatless were ' +beatless.join(' ');
+// console.log(sentence); 
+
+// concat - two arrays elements in one array
+// let older=['ringo','john'];
+// let younger=['paul','george'];
+// console.log(older + younger); 
+// console.log(older.concat(younger));
+
+// let relatedThings=[['windows','macos'],['new york', 'los']]
+// console.log(relatedThings[1][1])
+
+// nested array
+
+// let rsvp=[['ram','sita'],'john',['silly','maya','haina']];
+// for (let i=0;i<=rsvp.length-1;i++){
+//     let element=rsvp[i];
+//     if (Array.isArray(element)){
+//         for(let j=0;j<element.length;j++){
+//             console.log(element[j])
+//         }
+//     }
+//     else{
+//         console.log(element)
+//     }
+// }
+
+
+// let grid = [
+//     [1, 2, 3],
+//     [4, 5, 6], 
+//     [7, 8, 9]
+// ];
+
+// // Extract each column
+// let col1 = [];
+// let col2 = [];
+// let col3 = [];
+
+// for (let i = 0; i < grid.length; i++) {
+//     col1.push(grid[i][0]); // first column
+//     col2.push(grid[i][1]); // second column
+//     col3.push(grid[i][2]); // third column
+// }
+
+// console.log('Column 1:', col1); // [1, 4, 7]
+// console.log('Column 2:', col2); // [2, 5, 8]
+// console.log('Column 3:', col3); // [3, 6, 9]
+
+
+
+
+
+
+
+
+
+
+
